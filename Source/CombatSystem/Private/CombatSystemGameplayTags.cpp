1665435204497ca221ef2b67c70768fce50e7b69
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2025-11-12 20:02:28 +0300

    Added -> Gameplay Tags

---
+++ Source/CombatSystem/Private/CombatSystemGameplayTags.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CombatSystemGameplayTags.h"
+
+namespace CombatSystemGameplayTags
+{
+	//Input Tags
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+}
---
+++ Source/CombatSystem/Public/CombatSystemGameplayTags.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace CombatSystemGameplayTags
+{
+	//Input Tags
+
+	COMBATSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	COMBATSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
