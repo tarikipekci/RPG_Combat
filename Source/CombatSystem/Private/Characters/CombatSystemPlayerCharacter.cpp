7b80ab70e160c09e4112623997b9d52eb20c72a1
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2025-11-13 22:14:50 +0300

    Added -> Input Binding

--- Source/CombatSystem/Private/Characters/CombatSystemPlayerCharacter.cpp
+++ Source/CombatSystem/Private/Characters/CombatSystemPlayerCharacter.cpp
@@ -7,10 +7,16 @@
 #include "Components/CapsuleComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+#include "CombatSystemDebugHelper.h"
+#include "CombatSystemGameplayTags.h"
+#include "Components/Input/CombatSystemInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
 
 ACombatSystemPlayerCharacter::ACombatSystemPlayerCharacter()
 {
-	GetCapsuleComponent()->InitCapsuleSize(42.f,96.f);
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
@@ -18,7 +24,7 @@ ACombatSystemPlayerCharacter::ACombatSystemPlayerCharacter()
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(GetRootComponent());
 	CameraBoom->TargetArmLength = 200.0f;
-	CameraBoom->SocketOffset = FVector(0.f,55.f,65.f);
+	CameraBoom->SocketOffset = FVector(0.f, 55.f, 65.f);
 	CameraBoom->bUsePawnControlRotation = true;
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
@@ -26,13 +32,66 @@ ACombatSystemPlayerCharacter::ACombatSystemPlayerCharacter()
 	FollowCamera->bUsePawnControlRotation = false;
 
 	GetCharacterMovement()->bOrientRotationToMovement = true;
-	GetCharacterMovement()->RotationRate = FRotator(0.f,500.f,0.f);
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
 	GetCharacterMovement()->MaxWalkSpeed = 400.f;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+}
+
+void ACombatSystemPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	checkf(InputConfigDataAsset, TEXT("Forgot to assign a valid data asset as input config"));
+	ULocalPlayer* LocalPlayer = GetController<APlayerController>()->GetLocalPlayer();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+		UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext, 0);
+
+	UCombatSystemInputComponent* CombatSystemInputComp = CastChecked<UCombatSystemInputComponent>(PlayerInputComponent);
+	CombatSystemInputComp->BindNativeInputAction(InputConfigDataAsset, CombatSystemGameplayTags::InputTag_Move,
+	                                             ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+
+	CombatSystemInputComp->BindNativeInputAction(InputConfigDataAsset, CombatSystemGameplayTags::InputTag_Look,
+	                                             ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
 }
 
 void ACombatSystemPlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	Debug::Print(TEXT("Working"));
+}
+
+void ACombatSystemPlayerCharacter::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+	const FRotator MovementRotation(0.f, Controller->GetControlRotation().Yaw,0.f);
+
+	if (MovementVector.Y != 0.f)
+	{
+		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+	}
+
+	if (MovementVector.X != 0.f)
+	{
+		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void ACombatSystemPlayerCharacter::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+
+	if (LookAxisVector.X != 0.f)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+	}
+	if (LookAxisVector.Y != 0.f)
+	{
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
--- Source/CombatSystem/Public/Characters/CombatSystemPlayerCharacter.h
+++ Source/CombatSystem/Public/Characters/CombatSystemPlayerCharacter.h
@@ -3,12 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "CombatSystemDebugHelper.h"
 #include "Characters/CombatSystemBaseCharacter.h"
 #include "CombatSystemPlayerCharacter.generated.h"
 
+class UDataAsset_InputConfig;
 class USpringArmComponent;
 class UCameraComponent;
+struct FInputActionValue;
 /**
  * 
  */
@@ -16,10 +17,12 @@ UCLASS()
 class COMBATSYSTEM_API ACombatSystemPlayerCharacter : public ACombatSystemBaseCharacter
 {
 	GENERATED_BODY()
+
 public:
 	ACombatSystemPlayerCharacter();
 
 protected:
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 	virtual void BeginPlay() override;
 
 private:
@@ -30,5 +33,14 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
 	UCameraComponent* FollowCamera;
+#pragma endregion
+
+#pragma region Inputs
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="CharacterData", meta=(AllowPrivateAccess="true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
 #pragma endregion
 };
