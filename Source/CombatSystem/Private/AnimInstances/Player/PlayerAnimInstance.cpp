c3a2e688c138c96eee73167f4832e6a27323b62e
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2025-11-18 15:14:46 +0300

    Added -> Player Relaxed Idle Animation Sequence

--- Source/CombatSystem/Private/AnimInstances/Player/PlayerAnimInstance.cpp
+++ Source/CombatSystem/Private/AnimInstances/Player/PlayerAnimInstance.cpp
@@ -3,3 +3,30 @@
 
 #include "AnimInstances/Player/PlayerAnimInstance.h"
 
+#include "Characters/CombatSystemPlayerCharacter.h"
+
+void UPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningPlayerCharacter = Cast<ACombatSystemPlayerCharacter>(OwningCharacter);
+	}
+}
+
+void UPlayerAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+
+	if (bHasAcceleration)
+	{
+		IdleElapsedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		IdleElapsedTime += DeltaSeconds;
+		bShouldEnterRelaxState = IdleElapsedTime >= EnterRelaxStateThreshold;
+	}
+}
--- Source/CombatSystem/Public/AnimInstances/Player/PlayerAnimInstance.h
+++ Source/CombatSystem/Public/AnimInstances/Player/PlayerAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/CharacterAnimInstance.h"
 #include "PlayerAnimInstance.generated.h"
 
+class ACombatSystemPlayerCharacter;
 /**
  * 
  */
@@ -13,5 +14,22 @@ UCLASS()
 class COMBATSYSTEM_API UPlayerAnimInstance : public UCharacterAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData | References")
+	ACombatSystemPlayerCharacter* OwningPlayerCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="AnimData | LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="AnimData | LocomotionData")
+	float EnterRelaxStateThreshold = 5.f;
+
+	float IdleElapsedTime;
 };
