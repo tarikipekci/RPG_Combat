28af832708271a09000942b52025c83ecc6552bc
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2025-11-25 12:32:58 +0300

    Setting up weapon class

---
+++ Source/CombatSystem/Public/AbilitySystem/Abilities/CombatGameplayAbility.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "CombatGameplayAbility.generated.h"
+
+UENUM(BlueprintType)
+enum class ECombatAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+/**
+ * 
+ */
+UCLASS()
+class COMBATSYSTEM_API UCombatGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+	UPROPERTY(EditDefaultsOnly, Category="CombatAbility")
+	ECombatAbilityActivationPolicy AbilityActivationPolicy = ECombatAbilityActivationPolicy::OnTriggered;
+};
---
+++ Source/CombatSystem/Public/Items/Weapons/CombatPlayerWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Weapons/CombatWeaponBase.h"
+#include "CombatPlayerWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATSYSTEM_API ACombatPlayerWeapon : public ACombatWeaponBase
+{
+	GENERATED_BODY()
+	
+};
