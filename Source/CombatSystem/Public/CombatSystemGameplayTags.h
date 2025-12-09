50458fdcd88f9d8abb55d6d383d265243d5a7694
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2025-12-09 16:36:18 +0300

    Implemented -> Finisher attack logic

--- Source/CombatSystem/Public/CombatSystemGameplayTags.h
+++ Source/CombatSystem/Public/CombatSystemGameplayTags.h
@@ -25,4 +25,6 @@ namespace CombatSystemGameplayTags
 
 	COMBATSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Equip_Axe);
 	COMBATSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Event_Unequip_Axe);
+	
+	COMBATSYSTEM_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Status_JumpToFinisher);
 }
